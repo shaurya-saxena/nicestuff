5649f936259185c69e689603276ea1ca137d7f35
drinkwater <shauryasaxena.wrk@gmail.com>
2019-04-13 14:33:59 +0530

    Added hyperlink skeleton of webapp . Routes configured.

--- nicestuff/urls.py
+++ nicestuff/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('history', TemplateView.as_view(template_name='history.html'), name='history'),
+    path('favorites', TemplateView.as_view(template_name='favorites.html'), name='favorites'),
 
 ]   
 

c8f75c341af9e08361254a549fa9d1e46c7f9d01
cho <shauryasaxena.wrk@gmail.com>
2019-04-19 11:09:25 +0000

    URLs fixed for CSS issue . add static files next

--- recommender/urls.py
+++ recommender/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from django.views.generic.base import TemplateView # new
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('home/', views.home, name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('history', TemplateView.as_view(template_name='history.html'), name='history'),
+    path('favorites', TemplateView.as_view(template_name='favorites.html'), name='favorites'),
+
 ]
 
 

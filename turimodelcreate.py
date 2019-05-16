9edb9a237ee33a5d33a51fc17fff8024701048c2
drinkwater <shauryasaxena.wrk@gmail.com>
2019-05-16 14:10:26 +0530

    Script is a-okay. AlsoRemoved unnecessary CSVs.

--- turimodelcreate.py
+++ turimodelcreate.py
@@ -1,6 +1,6 @@
 import turicreate as tc
 
-tasteprofile = tc.SFrame.read_csv("TasteProfile.csv")
+tasteprofile = tc.SFrame.read_csv("TxnHistory10k.csv")
 
 m1 = tc.ranking_factorization_recommender.create(tasteprofile, 'user', 'pid', target='status')
 m1.save('recommendermodel')
